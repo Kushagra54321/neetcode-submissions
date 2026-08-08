84ebe0218fa5f80d76228b1b7242e05bab584eab
Kushagra Singh <149096668+Kushagra54321@users.noreply.github.com>
2026-08-08 18:42:25 +0530

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void rotate(vector<int>& arr, int k) {
+        int n=arr.size();
+        if (n == 0) return;
+
+        // Normalize k if greater than n
+        k = k % n;
+
+        // Store last k elements in a temporary array
+        vector<int>temp(k);
+        for (int i = n - k; i < n; i++) {
+            temp[i - n + k] = arr[i];
+        }
+
+        // Shift the first n-k elements to the right by k steps
+        for (int i = n - k - 1; i >= 0; i--) {
+            arr[i + k] = arr[i];
+        }
+
+        // Copy back the k elements to the start
+        for (int i = 0; i < k; i++) {
+            arr[i] = temp[i];
+        }
+
+    }
+};
